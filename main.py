7979f6f598327f0b80a1175f4ca41de1059fc67a
Alessandro Araujo <72277007+Xrlm-8@users.noreply.github.com>
2023-01-06 11:12:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-import pyautogui  #as = renomear
 from pyautogui import displayMousePosition
-
 displayMousePosition()
-
-pyautogui.alert('This is an alert box.')
-pyautogui.confirm('Shall I proceed?')
-pyautogui.confirm('Enter option.', buttons=['A', 'B', 'C'])
-pyautogui.prompt('What is your name?')
-pyautogui.password('Enter password (text will be hidden)')
-im1 = pyautogui.screenshot()
-button7location = pyautogui.locateOnScreen('button.png')
